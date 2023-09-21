850699f02d1c6431787210d2e03ff2d735cf02f4
Sakyto <procsssrus@mail.ru>
2023-09-22 01:27:41 +0400

    Add dropdown

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -18,6 +18,7 @@ const avatarToggle = React.forwardRef(({ children, onClick }, ref) => (
   </a>
 ));
 
+export default function Header({ showModal, account }) {
   return (
     <header className='headerPadding py-2 px-4 sticky-top text-white'>
       <div className='flex items-center justify-between'>
@@ -25,8 +26,21 @@ const avatarToggle = React.forwardRef(({ children, onClick }, ref) => (
           <p className='text-xl font-bold font-sans'>Portfolio</p>
           <p className='font-sans'>Цирк уехал, а клоуны остались</p>
         </div>
-        <div>
+        <div className='flex'>
+          
+          { account == null ? 
           <button className='px-3 py-1.5 border rounded-md transition ease-in-out text-gray-500 border-gray-500 hover:text-white hover:bg-gray-500' onClick={showModal}>Войти</button>
+          : 
+          <Dropdown>
+            <Dropdown.Toggle as={avatarToggle}/>
+          
+            <Dropdown.Menu>
+              <Dropdown.Item eventKey="profile" disabled>Профиль</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item eventKey="logout"><p className='text-red-700'>Выйти из аккаунта</p></Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+          }
         </div>
       </div>
     </header>
