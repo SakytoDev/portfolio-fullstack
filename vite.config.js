26d94201e5e5103aad1bcd90db90d55a3fe43b91
Konstantin Zasherinsky <procsssrus@mail.ru>
2023-09-27 17:43:10 +0400

    Fix: Render attempt #2

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-import fs from 'fs'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000,
-    host: '192.168.10.8',
-    https: {
-      key: fs.readFileSync('key.pem'),
-      cert: fs.readFileSync('cert.pem')
-    },
-  },
-  plugins: [react()],
+  plugins: [react()]
 })
