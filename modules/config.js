0c7dee7e71b339e52a7ba86793ecd5ea5be6c989
Konstantin Zasherinsky <procsssrus@mail.ru>
2023-09-28 23:16:44 +0400

    Switch to production mode

--- modules/config.js
+++ modules/config.js
@@ -3,7 +3,7 @@ import 'dotenv/config'
 const sessionSecret = process.env.SESSION_SECRET
 const passwordSalt = process.env.PASSWORD_SALT
 
-const isDev = true
+const isDev = false
 const hostname = "192.168.10.8"
 const port = 3000
 
