983828badc8b03c4a8e002fc32e2f6ef75cb6714
Konstantin Zasherinsky <procsssrus@mail.ru>
2023-10-10 22:57:36 +0400

    Improve UI and add portfolio card

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -47,24 +47,24 @@ export default function Header({ showModal }) {
   }
 
   return (
-    <header className='border-b-2 border-gray-600 p-4 sticky-top text-white backdrop-blur'>
+    <header className='border-b-2 border-gray-600 px-4 py-2.5 sticky-top text-white backdrop-blur'>
       <div className='flex items-center justify-between'>
         <div className='flex items-center'>
           <img className='w-12 h-12 transition-all ease-in-out duration-1000 hover:rotate-720 object-contain' src={logo}/>
           <p className='ms-2 text-xl font-bold font-sans'>Portfolio</p>
-          <a href='https://github.com/SakytoDev' target='_blank'><img className='ms-2 w-8 h-8' src={githubLogo}/></a>
+          <a className='ms-2' href='https://github.com/SakytoDev' target='_blank'><img className='w-8 h-8' src={githubLogo}/></a>
         </div>
         <div className='flex items-center'>
           { account == null ? 
-          <button className='px-4 py-2 border-2 rounded-md transition ease-in-out text-gray-500 border-gray-500 hover:text-white hover:bg-gray-500' onClick={showModal}>Войти</button>
+          <button className='px-4 py-2 border-2 rounded-md transition ease-in-out text-gray-500 border-gray-500 hover:text-white hover:bg-gray-500' onClick={showModal}>Login</button>
           : 
           <Dropdown>
             <Dropdown.Toggle as={avatarToggle}/>
           
             <Dropdown.Menu>
-              <Dropdown.Item eventKey="profile" disabled>Профиль</Dropdown.Item>
+              <Dropdown.Item eventKey="profile" disabled>Profile</Dropdown.Item>
               <Dropdown.Divider />
-              <Dropdown.Item eventKey="logout"><p className='text-red-700' onClick={() => logoutAccount()}>Выйти из аккаунта</p></Dropdown.Item>
+              <Dropdown.Item eventKey="logout"><p className='text-red-700' onClick={() => logoutAccount()}>Logout</p></Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
           }
--- client/src/pages/main.jsx
+++ client/src/pages/main.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux'
-import { login } from '../redux/accountSlice'
+import { Link } from 'react-router-dom';
 import { isMobile } from 'react-device-detect';
+import { login } from '../redux/accountSlice'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -13,7 +14,6 @@ import Input from '../components/input/input.jsx'
 import Typewriter from '../components/typewriter/typewriter';
 
 import Button from 'react-bootstrap/Button';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Modal from 'react-bootstrap/Modal';
 
 import Tab from 'react-bootstrap/Tab';
@@ -21,7 +21,11 @@ import Tabs from 'react-bootstrap/Tabs';
 
 import Spinner from 'react-bootstrap/Spinner';
 
-import chatLogo from '../assets/images/chatLogo.png'
+import chatLogo from '../assets/images/chatLogo.png';
+
+import websitelogo from '../assets/images/website.png';
+import androidlogo from '../assets/images/android.png';
+import windowslogo from '../assets/images/windows.png';
 
 export default function MainMenu({ sockets }) {
   const [modalShow, setModalShow] = useState(false)
@@ -85,36 +89,61 @@ export default function MainMenu({ sockets }) {
 
       <Header showModal={() => setModalShow(true)}/>
 
-      <h1 className='mt-5 text-center font-bold text-4xl md:text-5xl'>Проекты:</h1>
+      <div className='mx-8 md:mx-16 my-10'>
+        <div className='bg-gradient-to-br from-indigo-600 to-violet-900 flex flex-col md:grid md:grid-cols-[1fr,0.3fr] items-center rounded-3xl p-5'>
+          <div className='grid md:grid-rows-[1.1fr,1fr] md:p-5'>
+            <div className='flex justify-center md:justify-start gap-1 md:gap-3 text-[1.8rem] md:text-[4rem] font-black'>
+              <p>I'm</p>
+              <Typewriter delay={70} startDelay={200} infinite={true} text={['Frontend', 'Backend', 'Fullstack']}/>
+              <p>developer</p>
+            </div>
+            <div className='whitespace-pre-line'>
+              <Typewriter className='md:text-xl text-center md:text-left' startDelay={500} text={['I develop a variety of websites, including both single-page applications and multi-page websites, as well as integrating various APIs and databases.', '\nProgramming is my hobby. I am constantly looking for ways to improve my skills.']}/>
+            </div>
+          </div>
+          <div>
+            <img className='rounded-full w-64 h-64 mt-5 md:mt-0' src='https://avatars.githubusercontent.com/u/21197535?v=4'/>
+            <div className='p-2 mt-3 gap-2 border rounded-xl bg-gradient-to-b from-slate-700 to-slate-800 flex items-center justify-center'>
+              <img className='w-8 rounded transition-all ease-in-out hover:scale-125 hover:mx-1' src='https://www.mongodb.com/assets/images/global/favicon.ico'/>
+              <img className='rounded transition-all ease-in-out hover:scale-125 hover:mx-1' src='https://expressjs.com/images/favicon.png'/>
+              <img className='rounded transition-all ease-in-out hover:scale-125 hover:mx-1' src='https://react.dev/favicon.ico'/>
+              <img className='rounded transition-all ease-in-out hover:scale-125 hover:mx-1' src='https://nodejs.org/static/images/favicons/favicon.png'/>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <h1 className='mt-5 text-center font-bold text-4xl md:text-5xl'>Projects:</h1>
 
       <div className='rounded-3xl border-2 md:border-3 border-gray-300 mx-8 md:mx-16 mt-10'>
         <div className='flex items-center border-b-2 border-gray-300 p-5 md:p-10'>
           <img className='self-baseline w-16 h-16 md:w-24 md:h-24 rounded-2xl' src={chatLogo}/>
-          <div className='ms-5'>
-            <p className='text-3xl md:text-5xl font-bold'>MultiChat</p>
-            <p className='text-xl md:text-3xl mt-2'>Общайтесь с друзьями или находите новых</p>
+          <div className='ms-3 md:ms-5'>
+            <div className='flex gap-3'>
+              <p className='text-3xl md:text-5xl font-bold'>MultiChat</p>
+              <div className='flex gap-2 items-center'>
+                <img className='w-4 md:w-6' src={websitelogo}/>
+                <img className='w-4 md:w-6' src={windowslogo}/>
+                <img className='w-4 md:w-6' src={androidlogo}/>
+              </div>
+            </div>
+            <p className='text-xl md:text-3xl mt-2'>Chat with friends or find new ones</p>
           </div>
         </div>
 
         <div className='border-gray-300'>
           <div className='flex'>
             { !isMobile ? <div className='grid border-r border-gray-300 col p-7'>
-              <h2 className='text-3xl text-center font-bold'>Веб-версия</h2>
-              <ButtonGroup className="mt-3">
-                <Button variant="outline-light" href="/multichat">Войти</Button>
-              </ButtonGroup>
+              <h2 className='text-3xl text-center font-bold'>Web-version</h2>
+              <Link className='rounded-md border border-white flex items-center justify-center transition ease-in-out hover:bg-gray-300 hover:text-black' to='/multichat'>Enter</Link>
             </div> : null }
             { !isMobile ? <div className='grid border-gray-300 col p-7'>
               <h2 className='text-3xl text-center font-bold'>Windows</h2>
-              <ButtonGroup className="mt-3">
-                <Button variant="outline-light" disabled>В разработке</Button>
-              </ButtonGroup>
+              <Button className='mt-3' variant="outline-light" disabled>Coming soon</Button>
             </div> : null }
             { isMobile ? <div className='grid border-gray-300 col p-7'>
               <h2 className='text-3xl text-center font-bold'>Android</h2>
-              <ButtonGroup className='mt-3'>
-                <Button variant="outline-light" href="/download/android">Установить</Button>
-              </ButtonGroup>
+              <Button className='mt-3' variant="outline-light" href="/download/android">Download</Button>
             </div> : null }
           </div>
         </div>
@@ -124,29 +153,23 @@ export default function MainMenu({ sockets }) {
         <div className='flex items-center border-b-2 border-gray-300 p-10'>
           <div className='ms-5'>
             <p className='text-3xl md:text-5xl font-bold'>EvaPost</p>
-            <p className='text-xl md:text-3xl mt-2'>Поделитесь вашими мыслями о сегодняшнем дне, или расскажите, сколько кофе Вы сегодня выпили</p>
+            <p className='text-xl md:text-3xl mt-2'>Share your thoughts on public, or tell us how much coffee you drank today</p>
           </div>
         </div>
 
         <div className='border-gray-300'>
           <div className='flex'>
             { !isMobile ? <div className='grid border-r border-gray-300 col p-7'>
-              <h2 className='text-3xl text-center font-bold'>Веб-версия</h2>
-              <ButtonGroup className="mt-3">
-                <Button variant="outline-light" disabled>В разработке</Button>
-              </ButtonGroup>
+              <h2 className='text-3xl text-center font-bold'>Web-version</h2>
+              <Button className='mt-3' variant="outline-light" disabled>Coming soon</Button>
             </div> : null }
             { !isMobile ? <div className='grid border-gray-300 col p-7'>
               <h2 className='text-3xl text-center font-bold'>Windows</h2>
-              <ButtonGroup className="mt-3">
-                <Button variant="outline-light" disabled>В разработке</Button>
-              </ButtonGroup>
+              <Button className='mt-3' variant="outline-light" disabled>Coming soon</Button>
             </div> : null }
             { isMobile ? <div className='grid border-gray-300 col p-7'>
               <h2 className='text-3xl text-center font-bold'>Android</h2>
-              <ButtonGroup className='mt-3'>
-                <Button variant="outline-light" disabled>В разработке</Button>
-              </ButtonGroup>
+              <Button className='mt-3' variant="outline-light" disabled>Coming soon</Button>
             </div> : null }
           </div>
         </div>
@@ -156,29 +179,23 @@ export default function MainMenu({ sockets }) {
         <div className='flex items-center border-b-2 border-gray-300 p-10'>
           <div className='ms-5'>
             <p className='text-3xl md:text-5xl font-bold'>Trackify</p>
-            <p className='text-xl md:text-3xl mt-2'>Слушайте, делитесь или опубликуйте музыку</p>
+            <p className='text-xl md:text-3xl mt-2'>Listen, share or post music</p>
           </div>
         </div>
 
         <div className='border-gray-300'>
           <div className='flex'>
             { !isMobile ? <div className='grid border-r border-gray-300 col p-7'>
-              <h2 className='text-3xl text-center font-bold'>Веб-версия</h2>
-              <ButtonGroup className="mt-3">
-                <Button variant="outline-light" disabled>В разработке</Button>
-              </ButtonGroup>
+              <h2 className='text-3xl text-center font-bold'>Web-version</h2>
+              <Button className='mt-3' variant="outline-light" disabled>Coming soom</Button>
             </div> : null }
             { !isMobile ? <div className='grid border-gray-300 col p-7'>
               <h2 className='text-3xl text-center font-bold'>Windows</h2>
-              <ButtonGroup className="mt-3">
-                <Button variant="outline-light" disabled>В разработке</Button>
-              </ButtonGroup>
+              <Button className='mt-3' variant="outline-light" disabled>Coming soon</Button>
             </div> : null }
             { isMobile ? <div className='grid border-gray-300 col p-7'>
               <h2 className='text-3xl text-center font-bold'>Android</h2>
-              <ButtonGroup className='mt-3'>
-                <Button variant="outline-light" disabled>В разработке</Button>
-              </ButtonGroup>
+              <Button className='mt-3' variant="outline-light" disabled>Coming soon</Button>
             </div> : null }
           </div>
         </div>
@@ -188,31 +205,31 @@ export default function MainMenu({ sockets }) {
 
       <Modal show={modalShow} onHide={() => setModalShow(false)} centered>
         <Modal.Header className="justify-center">
-          <Modal.Title>Авторизация</Modal.Title>
+          <Modal.Title>Authorization</Modal.Title>
         </Modal.Header>
         <Modal.Body className="pt-1">
           <Tabs variant="underline" className="mt-1 justify-center" onSelect={handleTabChange}>
-            <Tab eventKey="accLogin" title="Логин">
+            <Tab eventKey="accLogin" title="Login">
               <div className="mt-3">
-                <Input type="text" name="nickname" placeholder="Никнейм" onChange={handleFormChange}/>
-                <Input className="mt-2" type="password" name="password" placeholder="Пароль" onChange={handleFormChange}/>
+                <Input type="text" name="nickname" placeholder="Nickname" onChange={handleFormChange}/>
+                <Input className="mt-2" type="password" name="password" placeholder="Password" onChange={handleFormChange}/>
               </div>
             </Tab>
-            <Tab eventKey="accReg" title="Регистрация">
+            <Tab eventKey="accReg" title="Register">
               <div className="mt-3">
-                <Input type="text" name="email" placeholder="Почта" onChange={handleFormChange}/>
-                <Input className="mt-2" type="text" name="nickname" placeholder="Никнейм" onChange={handleFormChange}/>
-                <Input className="mt-2" type="password" name="password" placeholder="Пароль" onChange={handleFormChange}/>
+                <Input type="text" name="email" placeholder="Email" onChange={handleFormChange}/>
+                <Input className="mt-2" type="text" name="nickname" placeholder="Nickname" onChange={handleFormChange}/>
+                <Input className="mt-2" type="password" name="password" placeholder="Password" onChange={handleFormChange}/>
               </div>
             </Tab>
           </Tabs>
           <Button variant="outline-primary" className="flex justify-center items-center w-100 mt-3" onClick={() => AuthToAccount()} disabled={authLoading}>
             { authLoading ? <Spinner className='me-2' animation="border" size="sm" /> : null }
-            { authMode == 'accLogin' ? 'Войти' : 'Регистрация' }
+            { authMode == 'accLogin' ? 'Sign in' : 'Sign up' }
           </Button>
         </Modal.Body>
         <Modal.Footer>
-          <Button className="w-100" variant="secondary" onClick={() => setModalShow(false)}>Закрыть</Button>
+          <Button className="w-100" variant="secondary" onClick={() => setModalShow(false)}>Close</Button>
         </Modal.Footer>
       </Modal>
     </>
