4dc3358b2fe0a67a9a0fc640c7bf049d72f0b0ac
Konstantin Zasherinsky <procsssrus@mail.ru>
2023-10-12 22:55:21 +0400

    Add new animation for cards

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -9,6 +9,15 @@ export default {
     extend: {
       rotate: {
         '720': '720deg',
+      },
+      keyframes: {
+        cardAppear: {
+          '0%': { transform: 'translateX(-300px)', opacity: 0 },
+          '100%': { transform: 'translateX(0px)', opacity: 100 }
+        }
+      },
+      animation: {
+        'card-appear': 'cardAppear 0.9s ease-in-out'
       }
     },
   },
