9a68fc21de239e74346e96cf94fbd3c7d1b0aa4d
Konstantin Zasherinsky <procsssrus@mail.ru>
2023-10-13 00:12:49 +0400

    Improve animations

--- client/src/pages/visitcard.jsx
+++ client/src/pages/visitcard.jsx
@@ -1,8 +1,32 @@
+import { motion } from 'framer-motion';
+
 import Typewriter from '../components/typewriter/typewriter';
 
 import multipage from '../assets/images/multipage.png';
 import crossplatform from '../assets/images/crossplatform.png';
 import backend from '../assets/images/backend.png';
+
+const skills = {
+  hidden: {
+    y: 50,
+    opacity: 0
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: { ease: 'easeInOut', duration: 1 }
+  }
+}
+
+const skillCard = {
+  hidden: { x: 200, opacity: 0 },
+  show: (custom) => ({ x: 0, opacity: 1, transition: { duration: 0.7, delay: custom * 0.2 } })
+}
+
+const skillLine = {
+  hidden: { width: '0%' },
+  show: { width: '50%', transition: { delay: 1.5, duration: 0.9 } }
+}
 
 export default function VisitCard() {
   return (
@@ -16,7 +40,7 @@ export default function VisitCard() {
               <p>developer</p>
             </div>
             <div className='whitespace-pre-line'>
-              <Typewriter className='md:text-xl text-center md:text-left' startDelay={1500} text={['I develop a variety of websites, including both single-page applications and multi-page websites, as well as integrating various APIs and databases.', '\nProgramming is my hobby. I am constantly looking for ways to improve my skills.']}/>
+              <Typewriter className='md:text-xl text-center md:text-left' startDelay={1500} text={['I develop a variety of websites, as well as integrating various APIs and databases.', '\nProgramming is my hobby. I am constantly looking for ways to improve my skills.']}/>
             </div>
           </div>
           <div>
@@ -31,25 +55,27 @@ export default function VisitCard() {
         </div>
       </div>
 
-      <h1 className='text-6xl font-black text-center'>My skills:</h1>
+      <motion.div initial='hidden' whileInView='visible' viewport={{ amount: 0.2, once: true }} variants={skills} className='mt-40'>
+        <h1 className='text-6xl font-black text-center'>My skills:</h1>
 
-      <div className='relative'>
-        <hr className='border-3 absolute w-50'/>
-        <div className='grid grid-cols-3 gap-20 mx-32 mt-16 mb-10'>
-          <div className='border-3 border-gray-500 rounded-xl p-5 flex flex-col items-center gap-3'>
-            <img className='h-16' src={multipage}/>
-            <p className='text-center font-medium'>Development of single-page and multi-page websites of any subject, landing page or simple online store. I also design websites using layouts from Figma or similar ones.</p>
-          </div>
-          <div className='border-3 border-gray-500 rounded-xl p-5 flex flex-col items-center gap-3'>
-            <img className='h-16' src={crossplatform}/>
-            <p className='text-center font-medium'>Adaptation and cross-platforming for any device and screen size.</p>
-          </div>
-          <div className='border-3 border-gray-500 rounded-xl p-5 flex flex-col items-center gap-3'>
-            <img className='h-16' src={backend}/>
-            <p className='text-center font-medium'>In addition to website layout, I develop server code and also connect databases.</p>
+        <div className='relative'>
+          <motion.hr initial='hidden' whileInView='show' variants={skillLine} viewport={{ amount: 0.2, once: true }} className='border-5 absolute -z-[1] top-[50%] left-[25%]'/>
+          <div className='grid grid-cols-3 gap-20 mx-32 mt-16 mb-10'>
+            <motion.div initial={{ x: 200, opacity: 0 }} whileInView='show' custom={3} variants={skillCard} viewport={{ amount: 0.35, once: true }} className='bg-zinc-950 border-2 border-gray-400 rounded-xl p-5 flex flex-col items-center gap-3'>
+              <img className='top-50 h-16' src={multipage}/>
+              <p className='text-center font-medium'>Development of single-page and multi-page websites of any subject, landing page or simple online store. I also design websites using layouts from Figma or similar ones.</p>
+            </motion.div>
+            <motion.div initial={{ x: 200, opacity: 0 }} whileInView='show' custom={4} variants={skillCard} viewport={{ amount: 0.35, once: true }} className='bg-zinc-950 border-2 border-gray-400 rounded-xl p-5 flex flex-col items-center gap-3'>
+              <img className='h-16' src={crossplatform}/>
+              <p className='text-center font-medium'>Adaptation and cross-platforming for any device and screen size.</p>
+            </motion.div>
+            <motion.div initial={{ x: 200, opacity: 0 }} whileInView='show' custom={5} variants={skillCard} viewport={{ amount: 0.35, once: true }} className='bg-zinc-950 border-2 border-gray-400 rounded-xl p-5 flex flex-col items-center gap-3'>
+              <img className='h-16' src={backend}/>
+              <p className='text-center font-medium'>In addition to website design, I develop server code and also connect databases.</p>
+            </motion.div>
           </div>
         </div>
-      </div>
+      </motion.div>
     </>
   )
 }
