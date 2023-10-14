b7759a5f76a5469e01563e2f395615825368f3cb
Konstantin Zasherinsky <procsssrus@mail.ru>
2023-10-14 16:02:46 +0400

    Temporary remove auth system

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -1,51 +1,11 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { logout } from '../../redux/accountSlice';
-
-import Dropdown from 'react-bootstrap/Dropdown';
 
 import logo from '../../assets/logo.png';
-import accIcon from '../../assets/images/defaultAcc.png';
 import githubLogo from '../../assets/images/github.png';
 
 import './header.css';
 
-import axios from 'axios';
-
-const avatarToggle = React.forwardRef(({ children, onClick }, ref) => (
-  <a
-    className='flex items-center dropdown-toggle'
-    href="#"
-    ref={ref}
-    onClick={(e) => {
-      e.preventDefault();
-      onClick(e);
-    }}
-  >
-    {children}
-    <img className='rounded-circle' src={accIcon} width="40" height="40"></img>
-  </a>
-));
-
-export default function Header({ showModal }) {
-  const account = useSelector((state) => state.auth.account)
-
-  const dispatch = useDispatch()
-
-  async function logoutAccount() {
-    const result = await axios({
-      url: '/api',
-      method: 'GET',
-      params: { type: 'accLogout', token: account.logoutToken }
-    })
-    .then(res => { return res.data })
-    .catch(err => { console.log(err) })
-  
-    if (result.code == 'success') {
-      dispatch(logout())
-    }
-  }
-
+export default function Header() {
   return (
     <header className='border-b-2 border-gray-600 px-4 py-2.5 sticky-top text-white backdrop-blur'>
       <div className='flex items-center justify-between'>
@@ -53,21 +13,6 @@ export default function Header({ showModal }) {
           <img className='w-12 h-12 transition-all ease-in-out duration-1000 hover:rotate-720 object-contain' src={logo}/>
           <p className='ms-2 text-xl font-bold font-sans'>Portfolio</p>
           <a className='ms-2' href='https://github.com/SakytoDev' target='_blank'><img className='w-8 h-8' src={githubLogo}/></a>
-        </div>
-        <div className='flex items-center'>
-          { account == null ? 
-          <button className='px-4 py-2 border-2 rounded-md transition ease-in-out text-gray-500 border-gray-500 hover:text-white hover:bg-gray-500' onClick={showModal}>Login</button>
-          : 
-          <Dropdown>
-            <Dropdown.Toggle as={avatarToggle}/>
-          
-            <Dropdown.Menu>
-              <Dropdown.Item eventKey="profile" disabled>Profile</Dropdown.Item>
-              <Dropdown.Divider />
-              <Dropdown.Item eventKey="logout"><p className='text-red-700' onClick={() => logoutAccount()}>Logout</p></Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-          }
         </div>
       </div>
     </header>
--- client/src/pages/main.jsx
+++ client/src/pages/main.jsx
@@ -1,120 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux'
-import { login } from '../redux/accountSlice'
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import axios from 'axios';
+import React from 'react';
 
 import Header from '../components/header/header.jsx';
 import Footer from '../components/footer/footer.jsx';
-import Input from '../components/input/input.jsx';
 import Tabs from '../components/tabs/tabs.jsx'
-
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-
-//import Tab from 'react-bootstrap/Tab';
-//import Tabs from 'react-bootstrap/Tabs';
-
-import Spinner from 'react-bootstrap/Spinner';
 
 import VisitCard from './visitcard';
 import Projects from './projects';
 
-export default function MainMenu({ sockets }) {
-  const [modalShow, setModalShow] = useState(false)
-  const [authForm, setForm] = useState({})
-  const [authMode, setAuthMode] = useState('accLogin')
-  const [authLoading, setAuthLoading] = useState(false)
-
-  const dispatch = useDispatch()
-
-  const handleTabChange = (tab) => { setAuthMode(tab) }
-
-  const handleFormChange = (e) => {
-    setForm({
-      ...authForm,
-      [e.target.name]: e.target.value
-    })
-  }
-
-  async function AuthToAccount() {
-    setAuthLoading(true)
-    
-    const result = await axios.get({
-      url: '/api',
-      params: { type: authMode, form: authForm }
-    })
-    .then(res => { return res.data })
-    .catch(err => { console.log(err) })
-    
-    if (result.code == 'success') {
-      await getAccount()
-
-      setModalShow(false)
-    }
-
-    setAuthLoading(false)
-  }
-
-  async function getAccount() {
-    const result = await axios.get({
-      url: '/api',
-      params: { type: 'getAcc' }
-    })
-    .then(res => { return res.data })
-    .catch(err => { console.log(err) })
-
-    if (result.code == 'success') {
-      sockets.emit('authUpdate', { "id": result.account.id })
-      dispatch(login({ "account": result.account }))
-    }
-  }
-
-  useEffect(() => {
-    getAccount()
-  }, [])
-
+export default function MainMenu() {
   return (
     <>
       <title>Portfolio</title>
 
-      <Header showModal={() => setModalShow(true)}/>
-
+      <Header/>
       <Tabs components={[<VisitCard title='About'/>, <Projects title='Projects'/>]}/>
-
       <Footer/>
-
-      {/* <Modal show={modalShow} onHide={() => setModalShow(false)} centered>
-        <Modal.Header className="justify-center">
-          <Modal.Title>Authorization</Modal.Title>
-        </Modal.Header>
-        <Modal.Body className="pt-1">
-          <Tabs variant="underline" className="mt-1 justify-center" onSelect={handleTabChange}>
-            <Tab eventKey="accLogin" title="Login">
-              <div className="mt-3">
-                <Input type="text" name="nickname" placeholder="Nickname" onChange={handleFormChange}/>
-                <Input className="mt-2" type="password" name="password" placeholder="Password" onChange={handleFormChange}/>
-              </div>
-            </Tab>
-            <Tab eventKey="accReg" title="Register">
-              <div className="mt-3">
-                <Input type="text" name="email" placeholder="Email" onChange={handleFormChange}/>
-                <Input className="mt-2" type="text" name="nickname" placeholder="Nickname" onChange={handleFormChange}/>
-                <Input className="mt-2" type="password" name="password" placeholder="Password" onChange={handleFormChange}/>
-              </div>
-            </Tab>
-          </Tabs>
-          <Button variant="outline-primary" className="flex justify-center items-center w-100 mt-3" onClick={() => AuthToAccount()} disabled={authLoading}>
-            { authLoading ? <Spinner className='me-2' animation="border" size="sm" /> : null }
-            { authMode == 'accLogin' ? 'Sign in' : 'Sign up' }
-          </Button>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button className="w-100" variant="secondary" onClick={() => setModalShow(false)}>Close</Button>
-        </Modal.Footer>
-      </Modal> */}
     </>
   )
 }
