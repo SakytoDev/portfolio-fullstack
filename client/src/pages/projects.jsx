ce9c6d692d7cf93604d2d8696b4d5ca232bdca98
Konstantin Zasherinsky <procsssrus@mail.ru>
2023-10-22 19:05:06 +0400

    Add projects themes

--- client/src/pages/projects.jsx
+++ client/src/pages/projects.jsx
@@ -23,6 +23,8 @@ const block = {
 export default function Projects() {
   return (
     <>
+      <h1 className='pb-10 text-6xl text-center font-black'>Services:</h1>
+
       <motion.div initial='hidden' whileInView='visible' variants={block} className='rounded-3xl border-2 md:border-3 border-gray-300 mx-8 md:mx-16 mb-10'>
         <div className='flex items-center border-b-2 border-gray-300 p-5 md:p-10'>
           <img className='self-baseline w-16 h-16 md:w-24 md:h-24 rounded-2xl' src={chatLogo}/>
@@ -58,7 +60,27 @@ export default function Projects() {
             </div> : null }
           </div>
         </div>
-      </motion.div> 
+      </motion.div>
+
+      <h1 className='pb-10 text-6xl text-center font-black'>Designs:</h1>
+      <motion.div initial='hidden' whileInView='visible' variants={block} className='rounded-3xl border-2 md:border-3 border-gray-300 mx-8 md:mx-16 mb-10'>
+        <div className='flex items-center border-b-2 border-gray-300 p-5 md:p-10'>
+          <div className='ms-3 md:ms-5'>
+            <div className='flex gap-2 md:gap-3'>
+              <p className='text-3xl md:text-5xl font-bold'>Noize MC</p>
+              <div className='md:border-2 rounded-lg flex items-center md:px-2 gap-2'>
+                <img className='w-4 md:w-6' src={websitelogo}/>
+              </div>
+            </div>
+            <p className='text-xl md:text-3xl mt-2'>An example of what a concert poster on a website would look like</p>
+          </div>
+        </div>
+        <div className='border-gray-300'>
+          <div className='p-5'>
+            <Link className='p-2 text-white rounded-md border-2 border-white flex items-center justify-center transition ease-in-out hover:bg-gray-300 hover:text-black' to='/noizemc'>Enter</Link>
+          </div>
+        </div>
+      </motion.div>
     </>
   )
 }
