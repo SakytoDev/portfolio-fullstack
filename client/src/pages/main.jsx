3a6ac64b28bcbefe6d63306c4917f4095022bbde
Konstantin Zasherinsky <procsssrus@mail.ru>
2023-10-22 18:20:08 +0400

    Make styles unique

--- client/src/pages/main.jsx
+++ client/src/pages/main.jsx
@@ -1,10 +1,10 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './main.css';
 
 import React from 'react';
 
 import Header from '../components/header/header.jsx';
 import Footer from '../components/footer/footer.jsx';
-import Tabs from '../components/tabs/tabs.jsx'
+import Tabs from '../components/tabs/tabs.jsx';
 
 import VisitCard from './visitcard';
 import Projects from './projects';
@@ -13,10 +13,12 @@ export default function MainMenu() {
   return (
     <>
       <title>Portfolio</title>
-
-      <Header/>
-      <Tabs components={[<VisitCard title='About'/>, <Projects title='Projects'/>]}/>
-      <Footer/>
+      
+      <div className='bg-gradient-to-t from-zinc-950 from-90% to-indigo-950 to-100% text-white'>
+        <Header/>
+        <Tabs className='flex justify-center gap-6 p-8' components={[<VisitCard title='About'/>, <Projects title='Projects'/>]}/>
+        <Footer/>
+      </div>
     </>
   )
 }
