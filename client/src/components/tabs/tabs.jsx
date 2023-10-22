9fb3cbde755f7f64961db795d2d07021f1f57205
Konstantin Zasherinsky <procsssrus@mail.ru>
2023-10-22 18:15:43 +0400

    Tabs: fix key issue with rendering lists

--- client/src/components/tabs/tabs.jsx
+++ client/src/components/tabs/tabs.jsx
@@ -1,23 +1,17 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
-export default function Tabs({ components }) {
+export default function Tabs({ className, components }) {
   const [tabIndex, setTabIndex] = useState(0)
-
-  const tabs = [];
-
-  components.forEach((component, index) => {
-    tabs.push(
-      <button className='group grid grid-rows-[1fr,0.5fr] p-3 font-medium' onClick={() => setTabIndex(index)}>
-        <p className='transition-all ease-in-out text-2xl'>{component.props.title}</p>
-        <div className={`bg-white rounded ${tabIndex == index ? 'ml-[0%] mr-[0%]' : 'ml-[100%] mr-[100%]'} mt-[6px] h-[3px] transition-all duration-500 ease-in-out group-hover:ml-[0%] group-hover:mr-[0%]`}></div>
-      </button>
-    )
-  })
 
   return (
     <div>
-      <div className='flex justify-center gap-2 py-5'>
-        {tabs}
+      <div className={className}>
+        { components.map((component, index) => {
+          return ( <button key={index} className='group grid grid-rows-[1fr,auto] font-medium' onClick={() => setTabIndex(index)}>
+            <p className='transition-all ease-in-out text-2xl'>{component.props.title}</p>
+            <div className={`bg-white rounded ${tabIndex == index ? 'ml-[0%] mr-[0%]' : 'ml-[100%] mr-[100%]'} mt-[6px] h-[3px] transition-all duration-500 ease-in-out group-hover:ml-[0%] group-hover:mr-[0%]`}></div>
+          </button> )
+        }) }
       </div>
       {components[tabIndex]}
     </div>
