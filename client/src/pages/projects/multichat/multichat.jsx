3095798fefebc489c97fb66e6c7f66ef179616ef
Konstantin Zasherinsky <procsssrus@mail.ru>
2023-10-27 18:52:26 +0400

    MultiChat: Update UI

--- client/src/pages/projects/multichat/multichat.jsx
+++ client/src/pages/projects/multichat/multichat.jsx
@@ -139,33 +139,31 @@ export default function MultiChat() {
     <>
       <title>MultiChat</title>
       { account != null ?
-      <div className='bg-black text-white h-screen grid grid-cols-[1fr,4fr] grid-rows-[auto,1fr] gap-x-[1px]'>
-        <div className='bg-[#212529] flex items-center p-2'>
-          <img className='bg-black rounded-full w-14 h-14' src={accIcon}/>
-          <div className='flex flex-col items-start mx-2 gap-1 max-w-[75%]'>
-            <p className='text-xl font-semibold truncate break-words w-full'>{account?.nickname}</p>
-            <Link className='border border-blue-500 rounded transition ease-in-out hover:bg-blue-600 px-2' to={`profile/${account?.id}`}>Profile</Link>
+      <div className='bg-black text-white grid grid-cols-[1fr,4fr] grid-rows-[100vh] gap-x-[1px]'>
+        <div className='grid grid-rows-[auto,1fr]'>
+          <div className='bg-[#212529] flex items-center p-2'>
+            <img className='bg-black rounded-full w-14 h-14' src={accIcon}/>
+            <div className='flex flex-col items-start mx-2 gap-1 max-w-[75%]'>
+              <p className='text-xl font-semibold truncate break-words w-full'>{account?.nickname}</p>
+              <Link className='border border-blue-500 rounded transition ease-in-out hover:bg-blue-600 px-2' to={`profile/${account?.id}`}>Profile</Link>
+            </div>
+          </div>
+          <div className='bg-[#2d3034] min-h-0 grid grid-rows-[1fr,auto]'>
+            <div className='flex flex-col overflow-auto gap-2 p-2'>
+              <Link className='border border-zinc-500 rounded p-2 text-lg text-center transition-all ease-in-out' to='chat'>Chat</Link>
+              <button className='border border-zinc-500 rounded p-2 text-lg transition-all ease-in-out'>Posts</button>
+              <button className='border border-zinc-500 rounded p-2 text-lg transition-all ease-in-out'>Music</button>
+            </div>
+            <div className='flex flex-col border-t-2 border-[#8f8f8f]'>
+              <button className='border border-red-500 rounded p-2 m-2 text-lg transition ease-in-out hover:bg-red-800' onClick={() => logoutAccount()}>Logout</button>
+            </div>
           </div>
         </div>
-        <div className='bg-[#212529] flex items-center justify-center'>
-          <p className='text-2xl font-bold'>Menu</p>
-        </div>
-        <div className='bg-[#2d3034] min-h-0 grid grid-rows-[1fr,auto]'>
-          <div className='flex flex-col overflow-auto gap-2 p-2'>
-            <Link className='border border-zinc-500 rounded p-2 text-lg text-center transition-all ease-in-out' to='chat'>Chat</Link>
-            <button className='border border-zinc-500 rounded p-2 text-lg transition-all ease-in-out'>Posts</button>
-            <button className='border border-zinc-500 rounded p-2 text-lg transition-all ease-in-out'>Music</button>
-          </div>
-          <div className='flex flex-col border-t-2 border-[#8f8f8f]'>
-            <button className='border border-red-500 rounded p-2 m-2 text-lg transition ease-in-out hover:bg-red-800' onClick={() => logoutAccount()}>Logout</button>
-          </div>
-        </div>
-        <div className='bg-[#1d2024] min-h-0 overflow-auto'>
-          <Routes>
-            <Route path='profile/:userId' element={<ProfileMenu socket={socket}/>}/>
-            <Route path='chat' element={<ChatMenu/>}/>
-          </Routes>
-        </div>
+        <Routes>
+          <Route path='profile/:userId' element={<ProfileMenu socket={socket}/>}/>
+          <Route path='chat' element={<ChatMenu/>}/>
+          <Route path='chat/:userId' element={<MessengerMenu socket={socket}/>}></Route>
+        </Routes>
       </div>
       :
       <div className='bg-[#2d3034] text-white h-[100vh] flex flex-col items-center pt-16'>
--- client/src/pages/projects/multichat/profilemenu.jsx
+++ client/src/pages/projects/multichat/profilemenu.jsx
@@ -41,23 +41,25 @@ export default function ProfileMenu({ socket }) {
 
   return (
     !updateLoading ?
-    <div className='grid grid-rows-[1fr,1fr] grid-cols-1 p-12'>
-      <div className='relative rounded-t-2xl border-2 border-b-[1px] border-white bg-gradient-to-tl from-violet-900 from-40% to-indigo-900 to-90%'>
-        <img className='bg-black w-32 h-32 border-[3px] border-white rounded-full absolute bottom-0 -right-[3%] translate-y-1/2 -translate-x-1/2 z-[1]' src={accIcon}/>
-      </div>
-      <div className='bg-slate-700 rounded-b-2xl border-2 border-t-[1px] border-white'>
-        <div className='flex flex-col items-start gap-3 p-5'>
-          <div className="bg-zinc-800 border-2 border-zinc-500 rounded-lg relative max-w-[80%]">
-            <p className='font-black text-5xl truncate break-words p-2 pb-3'>{account?.nickname}</p>
-            <div className={`absolute ${ !account?.isOnline ? 'bg-zinc-600' : 'bg-green-600' } rounded-full w-5 h-5 top-0 right-0 -translate-y-1/2 translate-x-1/2`}/>
-          </div>
-          <div className='ms-1'>
-            <p className={`font-medium ${ !account?.isOnline ? 'block' : 'hidden' }`}>Was online: { account?.lastLogin != null ? DateTime.local().toUTC().plus({ days: -DateTime.local().toUTC().diff(DateTime.fromISO(account.lastLogin), 'days').days}).toRelativeCalendar() : '' }</p>
-            <p className='font-medium'>Started the journey: { account?.dateCreated != null ? DateTime.fromISO(account?.dateCreated).toFormat('MMM dd, HH:mm') : ''}</p>
+    <div className='bg-[#2d3034] grid grid-rows-[auto,1fr]'>
+      <div className='grid grid-rows-[1fr,1fr] grid-cols-1 p-12'>
+        <div className='relative rounded-t-2xl border-2 border-b-[1px] border-white bg-gradient-to-tl from-violet-900 from-40% to-indigo-900 to-90%'>
+          <img className='bg-black w-32 h-32 border-[3px] border-white rounded-full absolute bottom-0 -right-[3%] translate-y-1/2 -translate-x-1/2 z-[1]' src={accIcon}/>
+        </div>
+        <div className='bg-slate-700 rounded-b-2xl border-2 border-t-[1px] border-white'>
+          <div className='flex flex-col items-start gap-3 p-5'>
+            <div className="bg-zinc-800 border-2 border-zinc-500 rounded-lg relative max-w-[80%]">
+              <p className='font-black text-5xl truncate break-words p-2 pb-3'>{account?.nickname}</p>
+              <div className={`absolute ${ !account?.isOnline ? 'bg-zinc-600' : 'bg-green-600' } rounded-full w-5 h-5 top-0 right-0 -translate-y-1/2 translate-x-1/2`}/>
+            </div>
+            <div className='ms-1'>
+              <p className={`font-medium ${ !account?.isOnline ? 'block' : 'hidden' }`}>Was online: { account?.lastLogin != null ? DateTime.local().toUTC().plus({ days: -DateTime.local().toUTC().diff(DateTime.fromISO(account.lastLogin), 'days').days}).toRelativeCalendar() : '' }</p>
+              <p className='font-medium'>Started the journey: { account?.dateCreated != null ? DateTime.fromISO(account?.dateCreated).toFormat('MMM dd, HH:mm') : ''}</p>
+            </div>
           </div>
         </div>
       </div>
-    </div> 
+    </div>
     : 
     null
   )
