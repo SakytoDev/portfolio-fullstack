e1369284a0e3fc19b9038f0e0fc634395dcd848b
Konstantin Zasherinsky <procsssrus@mail.ru>
2023-10-27 18:49:39 +0400

    Refactor code

--- client/src/pages/projects/multichat/chatmenu.jsx
+++ client/src/pages/projects/multichat/chatmenu.jsx
@@ -4,18 +4,18 @@ import { Link } from 'react-router-dom';
 
 import axios from 'axios';
 
-import accIcon from '../../../assets/images/defaultAcc.png';
+import accIcon from './assets/images/defaultAcc.png';
 
 function PeopleCard({ data }) {
   return (
     <div className='p-2 border-2 rounded-xl flex flex-col gap-2'>
       <div className='border-b-2 pb-2 flex items-center gap-2'>
-        <img className='w-12 h-12' src={accIcon}/>
+        <img className='bg-black rounded-full w-12 h-12' src={accIcon}/>
         <p className='text-xl'>{data.nickname}</p>
       </div>
       <div className='grid grid-cols-2 gap-2'>
         <Link className='border-2 p-1 border-indigo-400 rounded-lg text-lg text-center font-medium transition ease-in-out hover:bg-indigo-600' to={`../profile/${data._id}`}>View Profile</Link>
-        <button className='border-2 p-1 border-blue-400 rounded-lg text-lg font-medium transition ease-in-out hover:bg-blue-600'>Open Chat</button>
+        <Link className='border-2 p-1 border-blue-400 rounded-lg text-lg text-center font-medium transition ease-in-out hover:bg-blue-600' to={`../chat/${data._id}`}>Open Chat</Link>
       </div>
     </div>
   )
@@ -43,7 +43,7 @@ export default function ChatMenu() {
     .catch(err => { console.log(err) })
 
     if (result.code == 'success') {
-      setPeopleList(Object.values(result.accounts.friends))
+      setPeopleList(Object.values(result.friends.friends))
     }
   }
 
@@ -57,7 +57,7 @@ export default function ChatMenu() {
   }, [tabIndex])
 
   return (
-    <div>
+    <div className='bg-[#2d3034]'>
       <div className='px-12 py-4 border-b grid grid-cols-2'>
         <button className={`p-2 rounded-t text-xl font-bold text-center hover:bg-zinc-700 ${tabIndex == 0 ? 'border-b-2' : ''}`} onClick={() => setTabIndex(0)}>Find People</button>
         <button className={`p-2 rounded-t text-xl font-bold text-center hover:bg-zinc-700 ${tabIndex == 1 ? 'border-b-2' : ''}`} onClick={() => setTabIndex(1)}>Friends</button>
