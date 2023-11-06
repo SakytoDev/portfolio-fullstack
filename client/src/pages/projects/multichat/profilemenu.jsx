7b259f994177aa128a73c4d22440182a01d3558d
Konstantin Zasherinsky <procsssrus@mail.ru>
2023-11-06 21:25:12 +0400

    Improve profile UI

--- client/src/pages/projects/multichat/profilemenu.jsx
+++ client/src/pages/projects/multichat/profilemenu.jsx
@@ -59,30 +59,27 @@ export default function ProfileMenu({ socket }) {
 
   return (
     !updateLoading ?
-    <div className='bg-[#2d3034] grid grid-rows-[auto,1fr]'>
-      <div className='grid grid-rows-[1fr,1fr] p-12'>
-        <div className='relative rounded-t-2xl border-2 border-b-[1px] border-white bg-gradient-to-tl from-violet-900 from-40% to-indigo-900 to-90%'>
-          { selfAccount?.id == userID ?
-          <button className='absolute left-1/2 top-[100%] -translate-x-1/2 -translate-y-1/2 z-[1]' onClick={() => avatarFile.current.click()}>
-            <Avatar className='w-40 h-40 border-[3px] border-white' id={userID}/>
-            <input ref={avatarFile} onChange={handleAvatarChange} type='file' accept='image/*' className='hidden'></input>
-            <div className='bg-black group bg-opacity-0 rounded-full flex items-center justify-center absolute left-0 right-0 top-0 bottom-0 transition ease-in-out duration-300 hover:bg-opacity-80'>
-              <img className='w-16 h-16 transition ease-in-out duration-300 opacity-0 group-hover:opacity-100' src={editIcon}/>
-            </div>
-          </button> 
-          :
-          <Avatar className='w-40 h-40 border-[3px] border-white absolute left-1/2 top-[100%] -translate-x-1/2 -translate-y-1/2 z-[1]' id={userID}/> }
-        </div>
-        <div className='bg-slate-700 rounded-b-2xl border-2 border-t-[1px] border-white'>
-          <div className='flex flex-col items-start gap-3 p-5'>
-            <div className="bg-zinc-800 border-2 border-zinc-500 rounded-lg relative max-w-[80%]">
-              <p className='font-black text-5xl truncate break-words p-2 pb-3'>{account?.nickname}</p>
-              <OnlineIcon id={account?._id} socket={socket} className='absolute rounded-full w-5 h-5 top-0 right-0 -translate-y-1/2 translate-x-1/2'/>
-            </div>
-            <div className='ms-1'>
-              <OnlineText id={account?._id} socket={socket} onlineText='Online' onlineStyle='text-white' offlineText={`Was online: ${ account?.lastLogin ? DateTime.local().toUTC().plus({ days: -DateTime.local().toUTC().diff(DateTime.fromISO(account.lastLogin), 'days').days}).toRelativeCalendar() : ''}`} offlineStyle='text-white'/>
-              <p className='font-medium'>Started the journey: { account?.dateCreated != null ? DateTime.fromISO(account?.dateCreated).toFormat('MMM dd, HH:mm') : ''}</p>
-            </div>
+    <div className='bg-[#2d3034] py-6 grid grid-rows-[200px,1fr] grid-cols-[800px] justify-center'>
+      <div className='relative border-2 rounded-t-xl bg-gradient-to-tl from-violet-700 from-10% to-indigo-700 to-100%'>
+        { selfAccount?.id == userID ?
+        <button className='absolute left-1/2 top-[100%] -translate-x-1/2 -translate-y-1/2 z-[1]' onClick={() => avatarFile.current.click()}>
+          <Avatar className='w-40 h-40 border-[3px] border-white' id={userID}/>
+          <input ref={avatarFile} onChange={handleAvatarChange} type='file' accept='image/*' className='hidden'></input>
+          <div className='m-[3px] bg-black group bg-opacity-0 rounded-full flex items-center justify-center absolute left-0 right-0 top-0 bottom-0 transition ease-in-out duration-300 hover:bg-opacity-80'>
+            <img className='w-16 h-16 transition ease-in-out duration-300 opacity-0 group-hover:opacity-100' src={editIcon}/>
+          </div>
+        </button> 
+        :
+        <Avatar className='w-40 h-40 border-[3px] border-white absolute left-1/2 top-[100%] -translate-x-1/2 -translate-y-1/2 z-[1]' id={userID}/> }
+      </div>
+      <div className='border-t-0 border-2 rounded-b-xl'>
+        <div className='px-12 pt-24 flex flex-col items-stretch justify-center [&>*:first-child]:rounded-t-lg [&>*:last-child]:rounded-b-lg'>
+          <div className='bg-zinc-800 border-zinc-500 border-2'>
+            <p className='p-2 font-black text-5xl text-center truncate break-words'>{account?.nickname}</p>
+            <OnlineText className='pb-1 text-center text-lg' id={account?._id} socket={socket} onlineText='Online' onlineStyle='text-green-500' offlineText={`Was online: ${ account?.lastLogin ? DateTime.local().toUTC().plus({ days: -DateTime.local().toUTC().diff(DateTime.fromISO(account.lastLogin), 'days').days}).toRelativeCalendar() : ''}`} offlineStyle='text-gray-500'/>
+          </div>
+          <div className='bg-zinc-800 border-zinc-500 border-2 border-t-0'>
+            <p className='py-1 font-medium text-center'>Started the journey: { account?.dateCreated ? DateTime.fromISO(account.dateCreated).toFormat('MMM dd') : '' }</p>
           </div>
         </div>
       </div>
