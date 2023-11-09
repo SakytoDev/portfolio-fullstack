b57a9d9d892c9b12ae5df52d7a592d5dc5c4e878
Konstantin Zasherinsky <procsssrus@mail.ru>
2023-11-10 00:09:22 +0400

    Refactor code

--- modules/server.js
+++ modules/server.js
@@ -1,3 +1,5 @@
+const config = require('./config.js');
+
 const http = require('http');
 
 const express = require('express');
@@ -7,25 +9,23 @@ const app = express();
 const server = http.createServer(app);
 
 const { Server } = require('socket.io');
-const io = new Server(server, { cors: { origin: 'https://sakytodev.vercel.app' } });
+const io = new Server(server, { cors: { origin: config.isDev ? 'http://localhost:5173' : 'https://sakytodev.vercel.app' } });
 
 const session = require('cookie-session');
 const cookieParser = require('cookie-parser');
-
-const config = require('./config.js');
 
 module.exports = 
 {
     Setup : async function() {
         app.set('view engine', 'html')
 
-        app.use(cors({ origin: 'https://sakytodev.vercel.app' }))
+        app.use(cors({ origin: config.isDev ? 'http://localhost:5173' : 'https://sakytodev.vercel.app' }))
         app.use(cookieParser())
         app.use(session({
             secret: config.sessionSecret,
             saveUninitialized: true,
             resave: true,
-            cookie: { httpOnly: false, sameSite: 'none', secure: true, maxAge: 0 }
+            cookie: { sameSite: 'none', secure: true, httpOnly: false }
         }))
     
         if (config.isDev) {
