a61d1c381cde16b739eb77c3845b8cba465bf412
SakytoDev <procsssrus@mail.ru>
2024-01-17 22:08:15 +0400

    Limit request size

--- modules/server.js
+++ modules/server.js
@@ -21,6 +21,9 @@ module.exports =
         app.set('view engine', 'html')
         app.use(express.static(path.join(__dirname, '../client/dist')))
 
+        app.use(express.json({ limit: '12mb' }))
+        app.use(express.urlencoded({ extended: true }))
+
         app.use(cors())
         app.use(cookieParser())
         app.use(session({
